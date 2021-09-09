f7d010e0c78d6758f22cfc042f974ee0dfa46c7a
Alfonso Robles <fons3517@gmail.com>
2021-09-08 22:45:03 -0500

    updated javascript code: Added uvIndex function; changed bg colors; worked on localStorage.

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,8 @@
 var APIkey = "&appid=a462c584d163304aa73ed887092171dd&units=imperial";
-var queryUrl = "https://openweathermap.org/api/one-call-api";
+var queryUrl = "https://api.openweathermap.org/data/2.5/onecall?q=";
 var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?q=";
 var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q=";
+
 
 $('#search-city').on('click', function(event) {
     event.preventDefault();
@@ -9,6 +10,9 @@ $('#search-city').on('click', function(event) {
     console.log(cityName);
     currentWeatherForecast(cityName)
     fiveDayForecast(cityName)
+    localStorage.setItem("cityName", cityName);
+    document.getElementById('#enter-city').innerHTML = localStorage.getItem("cityName")
+
 });
 
 function currentWeatherForecast(cityName) {
@@ -18,14 +22,18 @@ function currentWeatherForecast(cityName) {
 
     }).then(function(APIresponse){
         console.log(APIresponse)
+        const lat = APIresponse.coord.lat
+        const lon = APIresponse.coord.lon
+        uvIndex(lat, lon)
         $("#current-weather").html(`
-        <div class='container bg-success'>
+        <div class='container bg-info'>
            <h3>City: ${cityName}</h3>
           <h6>Temp: ${APIresponse.main.temp}</h6>
            <h4>Description: ${APIresponse.weather[0].description}
            <img src='https://openweathermap.org/img/wn/${APIresponse.weather[0].icon}@2x.png'></h4>
             <h6>Humditity: ${APIresponse.main.humidity}%</h6>
             <h6>Wind speed: ${APIresponse.wind.speed}mph</h6>
+            <h6 class="uvIndex">UV Index:</h6>
         </div>
         `)
     })
@@ -41,7 +49,7 @@ function fiveDayForecast(cityName) {
         var codeHTML = "";
         for(let i = 0; i < APIresponse.list.length; i = i + 8){
             codeHTML += `
-            <div class='container bg-success'>
+            <div class='container bg-info'>
             <h3>City: ${cityName}</h3>
            <h6>Temp: ${APIresponse.list[i].main.temp}</h6>
             <h4>Description: ${APIresponse.list[i].weather[0].description}
@@ -55,17 +63,15 @@ function fiveDayForecast(cityName) {
     })
 }
 
-function uvIndex(cityName) {
+function uvIndex(lat, lon) {
     $.ajax({
-        url: queryUrl + cityName + APIkey, 
+        url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily${APIkey}&units=imperial`,
         method: "GET"
 
-    }).then(function(APIresponse){
-        console.log(APIresponse)
-        $('#current-weather').html(`
-            <div class='container bg-success'>
-            
+    }).then(uvIndexCallBack)
+}
 
-        `)
-    })
+function uvIndexCallBack(APIresponse){
+    console.log(APIresponse)
+    $(".uvIndex").text(`UV Index: ${APIresponse.current.uvi}`)
 }
